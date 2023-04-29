3f5b8ccc931b62eea4bde738fe90f188fa30f10f
Philipp Salvisberg <philipp.salvisberg@accenture.com>
2023-04-29 18:32:49 +0200

    install all API utPLSQL tests

--- install_test.sql
+++ install_test.sql
@@ -8,6 +8,21 @@ prompt =========================================================================
 prompt test_pinkdb.pks...
 @src/test/app/package/test_pinkdb.pks
 
+prompt test_departments.pks...
+@src/test/app/package/test_departments.pks
+
+prompt test_employees.pks...
+@src/test/app/package/test_employees.pks
+
+prompt test_department_salaries.pks...
+@src/test/app/package/test_department_salaries.pks
+
+prompt test_department_api.pks...
+@src/test/app/package/test_department_api.pks
+
+prompt test_employee_api.pks...
+@src/test/app/package/test_employee_api.pks
+
 prompt
 prompt ================================================================================================================
 prompt APP: test package bodies (from install_test.sql)
@@ -15,3 +30,18 @@ prompt =========================================================================
 
 prompt test_pinkdb.pkb...
 @src/test/app/package/test_pinkdb.pkb
+
+prompt test_departments.pkb...
+@src/test/app/package/test_departments.pkb
+
+prompt test_employees.pkb...
+@src/test/app/package/test_employees.pkb
+
+prompt test_department_salaries.pkb...
+@src/test/app/package/test_department_salaries.pkb
+
+prompt test_department_api.pkb...
+@src/test/app/package/test_department_api.pkb
+
+prompt test_employee_api.pkb...
+@src/test/app/package/test_employee_api.pkb
