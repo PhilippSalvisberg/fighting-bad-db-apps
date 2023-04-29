caa7cc21bbea4fb828a371f53a02dc1e0f7fb9dd
Philipp Salvisberg <philipp.salvisberg@accenture.com>
2023-04-29 21:15:46 +0200

    produce html result of tests and handle coverage issue

--- analyze.sh
+++ analyze.sh
@@ -1,3 +1,4 @@
+# run utPLSQL with code coverage
 utplsql run app_tester[app]/app_tester@xepdb1 \
     -source_path=src/main -owner=APP \
     -p='app' \
@@ -7,4 +8,12 @@ utplsql run app_tester[app]/app_tester@xepdb1 \
     -f=ut_sonar_test_reporter         -o=test_results.xml \
     -f=ut_junit_reporter              -o=junit_test_results.xml \
     -f=ut_documentation_reporter      -o=test_results.log -s
+
+# produce HTML result of JUnit results since SonarQube does not provide test details
+junit-viewer --results=junit_test_results.xml --save=junit_test_results.html
+
+# coverage.xml contains wrong lines when a package in src/main/app/package/alternative is active
+cp sound_coverage.xml coverage.xml
+
+# using sonar-project.properties 
 sonar-scanner
